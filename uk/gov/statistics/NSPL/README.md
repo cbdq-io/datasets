c4c9e47a9925db349cd33cdbdd2775e9ecf4d124
Ben Dalling <dallinb@users.noreply.github.com>
2026-01-02 17:25:51 +0000

    feat: postcode data (NSPL_NOV_2025)

---
+++ uk/gov/statistics/NSPL/README.md
@@ -0,0 +1,10 @@
+# National Statistics Postcode Lookup
+
+Extracted from <https://geoportal.statistics.gov.uk/datasets/8a1d5b58df824b2e86fe07ddfdd87165/about>.
+
+## Copyright
+
+- Contains OS data © Crown copyright and database right 2025
+- Contains Royal Mail data © Royal Mail copyright and Database right 2025
+- Contains GeoPlace data © Local Government Information House Limited copyright and database right 2025
+- Source: Office for National Statistics licensed under the Open Government Licence v.3.0
---
+++ uk/gov/statistics/NSPL/scripts/extract.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+#############################################################################
+# Extract CSV data from the Zip Archive downloaded from
+# <https://geoportal.statistics.gov.uk/datasets/8a1d5b58df824b2e86fe07ddfdd87165/about>.
+#############################################################################
+import argparse
+import logging
+import os
+import sys
+import zipfile
+
+from pathlib import Path
+
+PROG = os.path.basename(sys.argv[0])
+logging.basicConfig()
+logger = logging.getLogger(PROG)
+
+
+def main(input: str, output: str) -> None:
+    logger.debug(f'Creating "{output}"...')
+    p = Path(output)
+    p.mkdir()
+
+    with zipfile.ZipFile(input, 'r') as zip:
+        for input_name in zip.namelist():
+            if input_name.startswith('Data/multi_csv/'):
+                continue
+
+            if not input_name.endswith('.csv'):
+                continue
+
+            output_name = input_name \
+                .replace(' ', '_') \
+                .replace('(', '') \
+                .replace(')', '')
+            output_name = f'{output}/{output_name}'
+            logger.debug(f'Extracting "{input_name}" to "{output_name}"...')
+            dirname = os.path.dirname(output_name)
+            p = Path(dirname)
+            p.mkdir(parents=True, exist_ok=True)
+
+            with open(output_name, 'wb') as stream:
+                stream.write(zip.read(input_name))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog=PROG)
+    parser.add_argument(
+        '-d', '--debug',
+        help='Enable debug level logging.',
+        action='store_true',
+        required=False
+    )
+    parser.add_argument(
+        '-i', '--input',
+        help='The path to the downloaded Zip archive',
+        required=True
+    )
+    parser.add_argument(
+        '-o', '--output',
+        help='The path to the [not yet existing] directory to load the data.',
+        required=True
+    )
+    args = parser.parse_args()
+
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+        logger.debug('Log level is DEBUG')
+    else:
+        logger.setLevel(logging.INFO)
+
+    main(args.input, args.output)
